3aac02e07531cc61d507d957d752a27edd84d59b
Joao Henrique <joao.henrique1299@hotmail.com>
2023-05-30 16:23:15 -0300

    Colorschemes, sonokai is not configured in community.lua.

--- init.lua
+++ init.lua
@@ -17,8 +17,8 @@ return {
     },
   },
 
-  -- Set colorscheme to use
-  colorscheme = "astrodark",
+  -- Colorschemes.
+  colorscheme = "sonokai",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,11 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "sainnhe/sonokai",
+    init = function() -- init function runs before the plugin is loaded
+      vim.g.sonokai_style = "maia"
+    end,
+
+  },
 }
