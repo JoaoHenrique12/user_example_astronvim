83859da3a3cdeb9d75985c6eff33691384b57f56
Joao Henrique <joao.henrique1299@hotmail.com>
2023-06-02 07:29:33 -0300

    Adding my default set up for LSP

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -7,7 +7,10 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "lua_ls",
+        "pylsp",
+        "html",
+        "cssls",
+        "quick_lint_js",
       })
     end,
   },
@@ -18,8 +21,13 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "prettier",
+        "prettier",
         -- "stylua",
+        "isort",
+        "flake8",
+        "mypy",
+
+        "djlint",
       })
     end,
   },
@@ -29,7 +37,7 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "python",
+         "python",
       })
     end,
   },
