2ba303bc7aa71647f169cb10e039ba55e67e48da
Joao Henrique <joao.henrique1299@hotmail.com>
2024-01-29 22:51:53 -0300

    Add elixir and comment null-ls things.

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -7,31 +7,32 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        "pylsp",
+        "pyright",
         "html",
         "cssls",
         "lua_ls",
+        "elixirls",
       })
     end,
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
-  {
-    "jay-babu/mason-null-ls.nvim",
-    -- overrides `require("mason-null-ls").setup(...)`
-    opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        "prettier",
-        -- "stylua",
-        "isort",
-        "flake8",
-        "mypy",
-
-        "djlint",
-        "denols",
-      })
-    end,
-  },
+  -- {
+  --   "jay-babu/mason-null-ls.nvim",
+  --   -- overrides `require("mason-null-ls").setup(...)`
+  --   opts = function(_, opts)
+  --     -- add more things to the ensure_installed table protecting against community packs modifying it
+  --     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
+  --       "prettier",
+  --       -- "stylua",
+  --       "isort",
+  --       "flake8",
+  --       "mypy",
+  --
+  --       "djlint",
+  --       "denols",
+  --     })
+  --   end,
+  -- },
   {
     "jay-babu/mason-nvim-dap.nvim",
     -- overrides `require("mason-nvim-dap").setup(...)`
